2a3a891588f21c6966c5e3769179af919227a46a
alexthomas <alex.thomas@snet.net>
2022-06-08 11:10:55 -0400

    castling works I think

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Board import Board
 
 
 def validInput(p):
-    return len(p) == 2 and 65 <= ord(p[:1]) <= 72 and p[1:].isnumeric() and 1 <= int(p[1:]) <= 8
+    return (len(p) == 2 and 65 <= ord(p[:1]) <= 72 and p[1:].isnumeric() and 1 <= int(p[1:]) <= 8) or p == "castleR" or p == "castleL"
 
 
 team = 'W'
@@ -23,27 +23,28 @@ while not(gameBoard.isCheckmate('B') != False or gameBoard.isCheckmate('W') != F
                 if type(c).__name__ == "King" and c.team == team:  #finds King
                     tempBoard = gameBoard  #copies current board to tempBoard
                     if attempt == "castleL":
-                        if c.castleLeft(tempBoard):  #determines if board was able to castle left
+                        if c.castle('L', tempBoard):  #determines if board was able to castle left
                             gameBoard = tempBoard
                             if team == 'W':
                                 team = 'B'
                             else:
                                 team = 'W'
-                            continue  #for a successful castle, the gameBoard is updated, it becomes the other player's turn, and the rest of the code in the loop is skipped
+                            break  #for a successful castle, the gameBoard is updated, it becomes the other player's turn, and the rest of the code in the loop is skipped
                         else:
                             print("Conditions for castling left not met")
-                            continue  #for a failed castle, the gameBoard remains unchanged, it does NOT become the other player's turn, and the rest of the loop is skipped
+                            break  #for a failed castle, the gameBoard remains unchanged, it does NOT become the other player's turn, and the rest of the loop is skipped
                     elif attempt == "castleR":  #same as above code but for castling right
-                        if c.castleRight(tempBoard):
+                        if c.castle('R', tempBoard):
                             gameBoard = tempBoard
                             if team == 'W':
                                 team = 'B'
                             else:
                                 team = 'W'
-                            continue
+                            break
                         else:
                             print("Conditions for castling right not met")
-                            continue
+                            break
+        continue
 
 
     coords = [ord(attempt[:1]) - 65, 8 - int(attempt[1:])]
@@ -63,7 +64,7 @@ while not(gameBoard.isCheckmate('B') != False or gameBoard.isCheckmate('W') != F
 
     tempBoard = gameBoard
 
-    while not tempBoard.grid[coords[1]][coords[0]].move(mv[:1], mv[1:], tempBoard and not tempBoard.isCheck(team)):
+    while not tempBoard.grid[coords[1]][coords[0]].move(mv[:1], mv[1:], tempBoard) and not tempBoard.isCheck(team):
         tempBoard = gameBoard
         mv = input("That piece cannot move there, please select a valid position to move it to ")
     gameBoard = tempBoard
