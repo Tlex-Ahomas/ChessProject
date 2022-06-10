61db9792b229e81a564467516eaa78d4276525bb
3141five <whatifpotatoes@gmail.com>
2022-06-10 11:25:15 -0400

    CPU Start

--- CPU.py
+++ CPU.py
@@ -26,12 +26,35 @@ class CPU:
             for c in r:
                 if(tempBoard.grid[r][c].team == team): #Creates list of pieces CPU can move
                     pieces.appends(tempBoard.grid[r][c])
+        r = random.randint(len(pieces))
+        while(move == ""):
+            temp = filterForChecks(tempBoard, pieces[r].location)
+            if len(temp) ==0:
+                pieces.pop(r)
+            else:
+                r2 = randInt(len(temp))
+                pieces[r].move(temp[r2])
+                move = temp[r2]
+
 
 
         return move
 
     def filterForChecks(self, board, location):
-        return 0 #temporary
+        tempBoard = copy.deepcopy(board)
+
+        piece = tempBoard.grid[8 - int(location[1:])][ord(location[:1]) - 65]
+        moves = piece.calcPaths(tempBoard)
+        for m in piece.calcPaths(tempBoard):
+            piece.move(m[:1], m[:1], tempBoard)
+            if tempBoard.isCheck(team):
+                moves.remove(m)
+            tempBoard = copy.deepcopy(board)
+
+        return moves
+
+
+
 
 
 
