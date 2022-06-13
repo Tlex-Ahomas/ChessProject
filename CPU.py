77fdf98ba530634f5a4b6c82e36e09f296a7885e
3141five <whatifpotatoes@gmail.com>
2022-06-13 11:22:50 -0400

    start advanced cpu

--- CPU.py
+++ CPU.py
@@ -15,9 +15,38 @@ class CPU:
     def __init__(self, t, d):
         team = t
         difficulty = d # 0 or 1 for now
+        # 0 = bozo
+        # 1 = maybe competent
 
+    def makeSmartMove(self,board):
 
     def makeMove(self, board):
+        if self.difficulty == 0:
+            makeBozoMove(board)
+        else:
+            makeSmartMove(board)
+
+
+    def makeSmartMove(self,board): #This might take a minute
+        tempBoard = copy.deepcopy(board)
+        moves = []
+        pieces = []
+        move = ""
+        for r in tempBoard.grid:
+            for c in r:
+                if(tempBoard.grid[r][c].team == team): #Creates list of pieces CPU can move
+                    pieces.appends(tempBoard.grid[r][c])
+        r = random.randint(len(pieces))
+            while move == "":
+                r = random.randint(len(pieces))
+                temp = r.calcPaths()
+                while len(temp) > 0:
+                    r2 = randint(len(temp)
+
+
+
+
+    def makeBozoMove(self, board):
         tempBoard = copy.deepcopy(board)
         moves = []
         pieces = []
@@ -31,13 +60,11 @@ class CPU:
             temp = filterForChecks(tempBoard, pieces[r].location)
             if len(temp) ==0:
                 pieces.pop(r)
+                r = random.randint(len(pieces))
             else:
                 r2 = randInt(len(temp))
                 pieces[r].move(temp[r2])
                 move = temp[r2]
-
-
-
         return move
 
     def filterForChecks(self, board, location):
